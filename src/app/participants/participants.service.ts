b5f1eaa20902588692082a3506dfff5342b34068
JohnAsu <asucanchrist123@gmail.com>
2021-09-29 16:05:18 +0800

    first commit

---
+++ src/app/participants/participants.service.ts
@@ -0,0 +1,15 @@
+export class ParticipantsService {
+    getParticipants(){
+        return [
+            {name: "Protacia Asucan", age:69 + " years old", gender: "Female"},
+            {name: "Dhain Lore", age:28 + " years old", gender: "Male"},
+            {name: "Anastacia Maglaway", age:19 + " years old", gender: "Female"},
+            {name: "Benjamin Lasco", age:27 + " years old", gender: "Male"},
+            {name: "Tirso Cruz", age:61 + " years old", gender: "Male"},
+            {name: "Hipolito Kardo", age:38 + " years old", gender: "Male"},
+            {name: "Maxymiano Hidalgo", age:77 + " years old", gender: "Male"},
+            {name: "Susan Podador", age:57 + " years old", gender: "Female"},
+        ];
+    }
+
+}
